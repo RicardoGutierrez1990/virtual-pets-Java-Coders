e55bf329412a7ab537376a1c352067e3c4dd1a1c
Ricardo Gutierrez <Rick_1290@hotmail.com>
2021-01-29 14:26:56 -0500

    Back to square 1

--- src/main/java/virtual_pet/Application.java
+++ src/main/java/virtual_pet/Application.java
@@ -8,42 +8,13 @@ public class Application {
 
     public static void main(String[] args) {
         System.out.println("\nWelcome to Virtual Pet!");
-        System.out.println("Please select a Pet option (Please type 1 for ROBOTIC, 2 for ORGANIC, 3 QUIT GAME) ");
-
+        System.out.println("Please pick a name for your pet: ");
         Scanner inputScanner = new Scanner(System.in);
-        int petOption = inputScanner.nextInt();
-        inputScanner.nextLine();
-
-        do {
-            if (petOption == 3) {
-                System.out.println("See you next time!");
-                break;
-            } else if (petOption == 1) {
-                System.out.println("You've Selected a ROBOTIC pet option!");
-                System.out.println("Please input your new pet's name: ");
-                String playersPickedName = inputScanner.nextLine();
+        String petName = inputScanner.nextLine();
 
 
-                RoboticPet newRoboPet = new RoboticPet(playersPickedName, 10, 10, 10);
-                newRoboPet.greeting();
-                break;
-            } else if (petOption == 2) {
-                System.out.println("You've Selected an ORGANIC pet option!");
-                System.out.println("Please input your new pet's name: ");
-                String playersPickedName = inputScanner.nextLine();
-
-
-                OrganicPet newOrgPet = new OrganicPet(playersPickedName, 10, 10, 10);
-                newOrgPet.greeting();
-                break;
-            }
-
-            else {
-                System.out.println("This is not a valid option. Please try again.");
-                continue;
-            }
-
-        } while (petOption != 0 && petOption > 4);
+        virtualPet Pet = new virtualPet(petName, 10, 10, 10);
+        Pet.greeting();
 
     }
 }
---
+++ src/main/java/virtual_pet/virtualPet.java
@@ -0,0 +1,49 @@
+package virtual_pet;
+
+
+public class virtualPet {
+
+    private String name;
+    private int thirstLevel;
+    private int hungerLevel;
+    private int boredomLevel;
+
+
+    public virtualPet(String name, int thirstLevel, int hungerLevel, int boredomLevel){
+
+        this.name = name;
+        this.thirstLevel = thirstLevel;
+        this.hungerLevel = hungerLevel;
+        this.boredomLevel = boredomLevel;
+
+    }
+
+    public void greeting() {
+        System.out.println("Hi my name is " + name + ". Here are my current stats:\n"
+                +"\n"
+                +"Thirst Level:     |" + thirstLevel + "|\n"
+                + "Hunger Level:     |" + hungerLevel + "|\n"
+                + "Boredom Level:    |" + boredomLevel + "|\n"
+                +"\n"
+                + "Please take care of me!");
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int thirstLevel() {
+        return thirstLevel;
+    }
+
+    public int hungerLevel() {
+        return hungerLevel;
+    }
+
+    public int boredomLevel() {
+        return boredomLevel;
+    }
+
+
+}
+
